42124f08fdf420ffcfb6937cd94fe6a5336ca2d1
Favián Ramírez Jr <favianramirez2@hotmail.com>
2021-01-18 16:20:28 -0500

    Añado comentarios
    Añado una breve descripcion de que hace cada linea de codigo, posteriormente trabajaré con el resto de ejercicios

--- C++/Homeworks/2020B/Tarea3/Favian_Ramirez_Ejercicio1.cpp
+++ C++/Homeworks/2020B/Tarea3/Favian_Ramirez_Ejercicio1.cpp
@@ -1,44 +1,65 @@
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
+//Inclusion de librerias
+#include <iostream> //Libreria estandar de entrada y salida
+#include <cstdlib>  //Libreria de funciones estandar de c++
+#include <ctime>    //Libreria para tratamiento de fechas usando el primer segundo de UNIX como base
 
 using namespace std;
 
 int main(){
-    int n=0, aux;
+    //Declaracion de Variables
+    int n=0;    //Guarda el tamaño del vector 
+    int aux;    //Variable auxiliar para el reordenamiento del vector por Bubble Sort
+    int choose;
 
-    cout<<"Ingrese el n: ";
-    cin>>n;
-
-    int randomNumbers[n];
-
-    srand(time(NULL));
-
-    for (int i = 0; i <= n; i++)
+    do
     {
-        randomNumbers[i]=(rand()%(((3*n)+10)-(n)+1))+(n);
-    }
+        cout<< "Bienvenido a l programa Ejercicio 1 - Tarea 3: Vector de numeros aleatorios"<<endl;
 
-    for (int i=0; i<n; i++)
-    {
-        for (int j=i+1; j<n; j++)
+        //Ingreso de datos
+        cout<<"Ingrese el n: ";
+        cin>>n;
+
+        //Declaracion fuera de lugar npara capturar un n declarado
+        int randomNumbers[n]; //Almacena numeros aleatorios en el rango n a 3n+10
+
+        //Cambio de semilla de la funcion rand()
+        srand(time(NULL));  //Semilla seteada al numero de segundos transcurridos desde el primer segundo de UNIX
+
+        //Relleno el vector con numeros aleatorios en el rango
+        for (int i = 0; i < n; i++)
         {
-            if(randomNumbers[i]<randomNumbers[j])
+            randomNumbers[i]=(rand()%(((3*n)+10)-(n)+1))+(n);
+        }
+
+        //Reordeno el vector de mayor a menor 
+        for (int i=0; i<n; i++)
+        {
+            for (int j=i+1; j<n; j++)
             {
-                aux = randomNumbers[i];
-                randomNumbers[i] = randomNumbers[j];
-                randomNumbers[j] = aux;
+                if(randomNumbers[i]<randomNumbers[j])
+                {
+                    aux = randomNumbers[i];
+                    randomNumbers[i] = randomNumbers[j];
+                    randomNumbers[j] = aux;
+                }
             }
         }
-    }
-    for (int i = 0; i <= n; i++)
-    {
-        cout<< randomNumbers[i]<< "\t";
-    }
+
+        //Imprimo los valores en pantalla
+        for (int i = 0; i < n; i++)
+        {
+            cout<< randomNumbers[i]<< "\t";
+        }
+        
+        //Presentacion de resultados
+        cout<<"\n\nLa diferencia entre el valor minimo y el valor maximo es: "<<randomNumbers[0]-randomNumbers[n-1]<<endl;
+        cout<<"La suma de los 3 primeros numeros ordenados de forma descendente es: "<<randomNumbers[0]+randomNumbers[1]+randomNumbers[2]<<endl;
+
+        cout<<"Para repetir el programa ingrese un numero diferente de 0, para salir ingrese 0: ";
+        cin>>choose;
+
+    } while (choose);
+    cout<<"Saliendo...\t";
     
-    cout<<"\n\nLa diferencia entre el valor minimo y el valor maximo es: "<<randomNumbers[0]-randomNumbers[n]<<endl;
-    cout<<"La suma de los 3 primeros numeros ordenados de forma descendente es: "<<randomNumbers[0]+randomNumbers[1]+randomNumbers[2]<<endl;
-
-
     return 0;
 }
--- C++/Homeworks/2020B/Tarea3/Favian_Ramirez_Ejercicio2.cpp
+++ C++/Homeworks/2020B/Tarea3/Favian_Ramirez_Ejercicio2.cpp
@@ -1,26 +1,39 @@
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
+//Inclusion de librerias
+#include <iostream> //Libreria estandar de entrada y salida, tambien se puede usar stdio.h
+#include <cstdlib>  //Libreria de funciones estandar de C++
+#include <ctime>    //Libreria de manejo de fechas usando de base el primer segundo de Unix
 
 using namespace std;
 
 int main(){
-    int n;
+    //Declaracion de variables
+    int n;  //Longitud del vector randomNumbers
 
-    srand(time(NULL));
+    //Seteo de la semilla de la funcion rand()
+    srand(time(NULL));  //Semilla fijada al numero de segundos transcurridos desde el primer segundo de UNIX
 
+    //Ingreso de datos
     cout<<"Ingrese un entero n>5: ";
-    cin>>n;
-    int randomNumbers[(2*n)+3];
+    cin>>n; 
 
-    for (int i = 0; i <= (2*n)+3; i++)
+    //Declaracion de variable fuera de lugar pra capturar un n asignado
+    int randomNumbers[(2*n)+3]; //Vector que almacena numeros aleatorios
+
+    //Ciclo que rellena el vector con numeros aleatorios en el rango 10 a 1000
+    for (int i = 0; i < (2*n)+3; i++)
     {
         randomNumbers[i]=(rand()%((1000)-(10)+1))+(10);
     }
 
+    //Presentacion de resultados
     cout<<"El vector generado fue: "<<endl;
-    for (int i = 0; i <= (2*n)+3; i++)
+    for (int i = 0; i < (2*n)+3; i++)
     {
+        for (int j = 2; j < i; j++)
+        {
+            
+        }
+        
         cout<<randomNumbers[i]<<"\t";
     }
     
