04d22dbed1bc45b8ab87da205226dc54c8f5f443
Favián Ramírez Jr <favianramirez2@hotmail.com>
2021-01-18 17:12:49 -0500

    Añadidos Ejercicios hasta el 3 tarea 3
    Añado una Version Preliminar de los ejercicios 1, 2 y 3 de la tarea 3 de Programacion en C++

--- C++/Homeworks/2020B/Tarea3/Favian_Ramirez_Ejercicio2.cpp
+++ C++/Homeworks/2020B/Tarea3/Favian_Ramirez_Ejercicio2.cpp
@@ -8,35 +8,62 @@ using namespace std;
 int main(){
     //Declaracion de variables
     int n;  //Longitud del vector randomNumbers
+    bool prime=true;
+    int nPrimos=0;
 
     //Seteo de la semilla de la funcion rand()
     srand(time(NULL));  //Semilla fijada al numero de segundos transcurridos desde el primer segundo de UNIX
 
-    //Ingreso de datos
-    cout<<"Ingrese un entero n>5: ";
-    cin>>n; 
+    cout<<"Bienvenido al programa Ejercicio 2 - Tarea 3: Numeros primos en el vector aleatorio"<<endl;
 
-    //Declaracion de variable fuera de lugar pra capturar un n asignado
-    int randomNumbers[(2*n)+3]; //Vector que almacena numeros aleatorios
-
-    //Ciclo que rellena el vector con numeros aleatorios en el rango 10 a 1000
-    for (int i = 0; i < (2*n)+3; i++)
+    do
     {
-        randomNumbers[i]=(rand()%((1000)-(10)+1))+(10);
-    }
-
-    //Presentacion de resultados
-    cout<<"El vector generado fue: "<<endl;
-    for (int i = 0; i < (2*n)+3; i++)
-    {
-        for (int j = 2; j < i; j++)
+        //Ingreso de datos
+        cout<<"Ingrese un entero n>5 o 0 para salir: ";
+        cin>>n; 
+        if (n>5)
         {
-            
+            //Declaracion de variable fuera de lugar pra capturar un n asignado
+            int randomNumbers[(2*n)+3]; //Vector que almacena numeros aleatorios
+
+            //Ciclo que rellena el vector con numeros aleatorios en el rango 10 a 1000
+            for (int i = 0; i < (2*n)+3; i++)
+            {
+                randomNumbers[i]=(rand()%((1000)-(10)+1))+(10);
+            }
+
+            //Presentacion de resultados
+            cout<<"El vector generado fue: "<<endl;
+            for (int i = 0; i < (2*n)+3; i++)
+            {
+                prime= true;    //El indicador se resetea a verdadero cada vez que se va a comprobar un numero, así me ahorro un condicional
+                for (int j = 2; j < randomNumbers[i] && prime; j++) //La condicion adicional es para dejar de buscar una vez se encontró un divisior exacto
+                {
+                    if(!(randomNumbers[i]%j)){  //La expresion evalua verdadero cuando el modulo es 0
+                        prime=false;
+                    }
+                }
+                if (prime)
+                {
+                    cout<<"\t"<<randomNumbers[i]<<" Es Primo!"<<endl;
+                    nPrimos++;
+                }else
+                {
+                    cout<<"\t"<<randomNumbers[i]<<endl;
+                }
+                
+            }
+            cout<<"\nEl Total de Numeros Primos en el vector fue de: "<<nPrimos<<endl;
+            cout<<"\nPara terminar la ejecucion del codigo digite 0, para volver a ejecutar ingrese cualquier numero diferente de 0: ";
+            cin>>n;
+        }else if (n<=5 && n>0)
+        {
+            cout<<"Valor Ingresado Incorrecto, intente de nuevo"<<endl;
         }
-        
-        cout<<randomNumbers[i]<<"\t";
-    }
     
+    } while (n);
+
+    cout<<"Saliendo... "<<endl;
 
     return 0;
 }
--- C++/Homeworks/2020B/Tarea3/Favian_Ramirez_Ejercicio3.cpp
+++ C++/Homeworks/2020B/Tarea3/Favian_Ramirez_Ejercicio3.cpp
@@ -1,8 +1,70 @@
-#include <iostream>
+//Inclusion de Librerias
+#include <iostream> //Libreria estandar de entrada y salida
+#include <ctime>    //Libreria estandar de manejo de fechas usando de base el primer segundo de UNIX
+#include <cstdlib>  //Libreria de Funciones Estandar de C++
 
 using namespace std;
 
 int main(){
+    //Declaracion de variables Globales
+    int n;
 
+    cout<<"Bienvenido al programa Ejercicio 3 - Tarea 3: Reordenamiento del Vector"<<endl;
+    do
+    {
+        cout<<"\nIngresa un valor n>3 para que sea la longitud del vector a general o 0 para salir: ";
+        cin>>n;
+
+        if (n>3)
+        {
+            //Declaracion de variables locales
+            double aux;
+            double randomNumbers[n];
+
+            //Cambio de semilla de la funcion rand()
+            srand(time(NULL));  //Semilla seteada al numero de segundos transcurridos desde el primer segundo de UNIX
+
+            //Relleno el vector con numeros aleatorios en el rango
+            for (int i = 0; i < n; i++)
+            {
+                randomNumbers[i]=double((rand()%(4765-(-2143)+1))+(-2143))/1000.0; //Divido para un 1 seguido de tantos 0 como decimales quiero
+            }
+
+            cout<<"El vector sin ordenar es: "<<endl;
+            for (int i = 0; i < n; i++)
+            {
+                cout<< randomNumbers[i]<< "\t";
+            }
+
+            //Reordeno el vector de mayor a menor 
+            for (int i=0; i<n; i++)
+            {
+                for (int j=i+1; j<n; j++)
+                {
+                    if(randomNumbers[i]<randomNumbers[j])   //Compruebo si el vector es menor
+                    {
+                        aux = randomNumbers[i];             //En caso de ser menor lo guardo en mi variable auxiliar
+                        randomNumbers[i] = randomNumbers[j];//Intercambio los valores de posicion
+                        randomNumbers[j] = aux;
+                    }
+                }
+            }
+
+            cout<<"\n\nEl vector ordenado es: "<<endl;
+
+            //Imprimo los valores en pantalla
+            for (int i = 0; i < n; i++)
+            {
+                cout<< randomNumbers[i]<< "\t";
+            }
+
+        }else if (n<3 && n>0)
+        {
+            cout<<"\nValor ingresado Invalido, intente de nuevo"<<endl;
+        }
+        
+    } while (n);
+    
+    cout<<"\nSaliendo... "<<endl;
     return 0;
 }
